78ff24205615ff20f58663a0467fbe968b3df65c
GeorgiDg <33101215+Georgidg@users.noreply.github.com>
2018-06-15 00:47:12 +0300

    Add files via upload

---
+++ TestCase.cpp
@@ -0,0 +1,13 @@
+#include "TestCase.hpp"
+
+TestCase::TestCase(const string str, ostream &outF) : str(str), outF(outF)
+{
+    Test1 = 0;
+    Test2 = 0;
+    counterTests = 0;
+}
+void TestCase::print()
+{
+    outF << str << ": " << Test2 << " failed, " << Test1 << " passed, " << counterTests << " final_num." << endl
+      << "-----" << endl;
+}
---
+++ TestCase.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class TestCase
+{
+  private:
+    string str;
+    int Test1;
+    int Test2;
+    int counterTests;
+    ostream &outF;
+	
+  public:
+    void print();
+    TestCase(const string str, ostream &outF);
+    template <typename T>
+    TestCase check_equal(T a, T b)
+    {
+        counterTests++;
+        if (!(a == b))
+        {
+            Test2++;
+            outF << str << ": Failure in test #" << counterTests << ": "
+              << a << " should equal " << b << "!" << endl;
+        }
+        else
+            Test1++;
+        return *this;
+    }
+    template <typename T1, typename T2, typename T3>
+    TestCase check_function(T1(f), T2 a, T3 b)
+    {
+        counterTests++;
+        if (!(f(a) == b))
+        {
+            Test2++;
+            outF << s << ": Failure in test #" << counterTests << ": Function should return " << b << " but returned " << f(a) << "!" << endl;
+        }
+        else
+            Test1++;
+        return *this;
+    }
+}  
+    template <typename T>
+    TestCase check_output(T a, string givenStr)
+    {
+        counterTests++;
+        ostringstream OS_STR;
+        OS_STR << a;
+        string tests = OS_STR.str();
+        if (tests != givenStr)
+        {
+            Test2++;
+            outF << str << ": Failure in test #" << counterTests << ": Expect string value to be "
+              << givenStr << " but is " << a << endl;
+        }
+        else
+            Test1++;
+        return *this;
+    }
+  
+template <typename T>
+    TestCase check_different(T a, T b)
+    {
+        counterTests++;
+        if (!(a != b))
+        {
+            Test2++;
+            outF << str << ": Failure in test #" << counterTests << ": "
+              << a << " should different " << b << "!" << endl;
+        }
+        else
+            Test1++;
+        return *this;
+    }
+;
